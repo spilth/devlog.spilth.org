6162884a922a24bac24dd95e9b672e2a74e18f65
Brian Kelly <polymonic@gmail.com>
2012-12-06 16:05:26 -0500

    Ignoring blank lines

--- devlog.rb
+++ devlog.rb
@@ -3,25 +3,28 @@
 require 'date'
 
 while (text = gets) do
-  now = DateTime.now
-  filename = now.strftime("_posts/%Y-%m-%d-devlog.md")
+  if text.chomp != ""
+    now = DateTime.now
+    filename = now.strftime("_posts/%Y-%m-%d-devlog.md")
 
-  if (!File.exists?(filename))
-    title = now.strftime("%Y-%m-%d")
-    file = File.open(filename, 'w') 
-    file.puts <<eos
+    if (!File.exists?(filename))
+      title = now.strftime("%Y-%m-%d")
+      file = File.open(filename, 'w') 
+      file.puts <<eos
 ---
 title: #{title}
 layout: default
 ---
 eos
 
-  else
-    file = File.open(filename, 'a')
+    else
+      file = File.open(filename, 'a')
+    end
+
+    file.puts "- #{text}"
+
+    file.close
   end
-
-  file.puts "- #{text}"
-
-  file.close
 end
+
 
